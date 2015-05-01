b44d24f70a3ff8ead7f96a2379e9816e33696791
brook <brookthomas@gmail.com>
2015-05-01 10:22:39 -0700

    added ability for frame to swap between bet mode and discard mode. complete refactoring of many other parts of the class

--- views/InputView.java
+++ views/InputView.java
@@ -8,58 +8,101 @@ import models.*;
 
 public class InputView extends JPanel {
 
+	// Betting Controls and Readout
+	JButton decrementButton = new JButton("-1");
+	JButton incrementButton = new JButton("+1");
+	JLabel betAmount = new JLabel("1");
+
+	// Listeners
+	MouseListener decrementListener = new DecrementBetOnClick();
+	MouseListener incrementListener = new IncrementBetOnClick();
+	MouseListener betListener = new PlaceBetOnClick();
+	MouseListener discardListener = new DiscardCardsOnClick();
+
+	//PlaceBet Button
+	JButton userActionButton = new JButton();
+
+	int currentBetAmount = 1;
+
+	// vars established in constructor
 	MainGameFrame gameFrame;
 	Player player;
 
-	JButton decrementButton, incrementButton, placeBetButton, discardCardsButton;
-	JLabel betAmount;
-
-	int currentBetAmount = 1;
-	int currentPlayerBalance;
-
 	public InputView (MainGameFrame gameFrame, Player player) {
-		// assign vars
+
+		// Assign variables
 		this.gameFrame = gameFrame;
 		this.player = player;
 
-		// configure frame layout
+		//	Configure frame layout along x-axis
 		setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
 		setPreferredSize(new Dimension(800,100));
 
-		// configure decrement button
-		decrementButton = new JButton("-1");
-		decrementButton.addMouseListener(new DecrementBetOnClick());
-
-		// configure betAmount label
-		betAmount = new JLabel(String.valueOf(currentBetAmount));
-
-		// configure increment button
-		incrementButton = new JButton("+1");
-		incrementButton.addMouseListener(new IncrementBetOnClick());
-
-		// configure place bet button
-		placeBetButton = new JButton("Place Bet");
-		placeBetButton.addMouseListener(new PlaceBetOnClick());
+		// Intial Configuration for ActionButton
+		// Note: this also sets up the betting buttons
+		setUserActionButtonBetMode();
 
 		// add elements to frame
 		add(decrementButton);
 		add(betAmount);
 		add(incrementButton);
-		add(placeBetButton);
+		add(userActionButton);
 
+	}
+
+	public void setUserActionButtonBetMode () {
+
+		// Swap discardListener for betListener
+		userActionButton.removeMouseListener(discardListener);
+		userActionButton.addMouseListener(betListener);
+
+		enableBettingButtons();
+
+		userActionButton.setText("Place Bet");
+
+	}
+
+	public void setUserActionButtonDiscardMode () {
+
+		// Swap betListener for discardListener
+		userActionButton.removeMouseListener(betListener);
+		userActionButton.addMouseListener(discardListener);
+
+		disableBettingButtons();
+
+		userActionButton.setText("Discard Cards");
+
+	}
+
+	public void disableBettingButtons() {
+		/* Nota bene - disabling a button does NOT disable the
+			underlying mouse or event listeners. Gotta pull
+			those separately. */
+
+		decrementButton.removeMouseListener(decrementListener);
+		incrementButton.removeMouseListener(incrementListener);
+
+		decrementButton.setEnabled(false);
+		incrementButton.setEnabled(false);
+	}
+
+	public void enableBettingButtons() {
+		decrementButton.addMouseListener(decrementListener);
+		incrementButton.addMouseListener(incrementListener);
+
+		decrementButton.setEnabled(true);
+		incrementButton.setEnabled(true);
 	}
 
 
 	class IncrementBetOnClick extends MouseInputAdapter {
-		/* this class will increment the bet amount and update
-			the relevant display, if permissable, upon every click
-			of incrementButton */
+		/* Increments currentBetAmount by 1 up to 
+			a maximum of the player's current balance */
 
 		@Override
 		public void mouseClicked (MouseEvent e) {
 
-			if (currentBetAmount < currentPlayerBalance)
-			{
+			if (currentBetAmount < player.getBalance()) {
 				currentBetAmount+= 1;
 				betAmount.setText(String.valueOf(currentBetAmount));
 			}
@@ -67,15 +110,13 @@ public class InputView extends JPanel {
 	}
 
 	class DecrementBetOnClick extends MouseInputAdapter {
-		/* this class will decrement the bet amount and update
-			the relevant display, if permissable, upon every click
-			of decrementButton */
+		/* Decrements currenBetAmount by 1 down to
+			a minimum of 1 */
 
 		@Override
 		public void mouseClicked (MouseEvent e) {
 
-			if (currentBetAmount >= 2)
-			{
+			if (currentBetAmount >= 2) {
 				currentBetAmount-= 1;
 				betAmount.setText(String.valueOf(currentBetAmount));
 			}
@@ -83,14 +124,27 @@ public class InputView extends JPanel {
 	}
 
 	class PlaceBetOnClick extends MouseInputAdapter {
-		/* this class will close out the getBet loop and return a bet
-			to the controller when placeBetButton is clicked */
+		/* Sends currentBetAmount to Controller and changes
+			this frame's layout to Discard mode */
 
 		@Override
 		public void mouseClicked (MouseEvent e) {
+			setUserActionButtonDiscardMode();
 			gameFrame.relayBetAmountToController(currentBetAmount);
 		}
 
+	}
+
+	class DiscardCardsOnClick extends MouseInputAdapter {
+		/* this class signals PokerGame by way of MainGameFrame
+		that the user is done with selecting discards */
+
+		@Override
+		public void mouseClicked (MouseEvent e) {
+			setUserActionButtonBetMode();
+			// holding pattern for now
+			System.out.println("DiscardCardsOnClick() called");
+		}
 	}
 
 
