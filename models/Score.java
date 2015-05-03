050c83e7a344fc118547499035718a478f9d1098
Nimitha Ramesh <nimitha.r.ramesh@gmail.com>
2015-05-03 12:29:43 -0700

    Added a score multiplier method

--- models/Score.java
+++ models/Score.java
@@ -6,21 +6,221 @@ class Score{
   private int[] score;
   private final static int MAX_SIZE = 5;
   private Hand myCards;
+  private int[] cardRanks;
   private int[] cardValues;
-  //private Dictionary myDictionary={ca = 100*1, c2= 100*2};
+  private Player user;
+
+  //Note to self: Call numberOfMethods() wherever you use cardValues!@!!!!@!!!!!
 
   public Score(){
     this.myCards=new Hand();
-    int[] cardRanks= new int[5];
+    user = new Player(100,myCards);
+    cardRanks= new int[5];
+    cardValues=new int[13];
     for(int i=0; i<5; i++){
       cardRanks[i] = myCards.getCard(i).getRank();
     }
-    score=new int[MAX_SIZE];
-    score=numberOfCards(myCards);
+    //score=new int[MAX_SIZE];
+    //score=numberOfCards(myCards);
   }
 
+  public int scoreMultiplier(){
+    if(user.hand.royalFlush()){
+      user.balance*=25;
+    }
+    else if(user.hand.straightFlush()){
+      user.balance*=20;
+    }
+    else if(user.hand.fourOfAKind()){
+      user.balance*=15;
+    }
+    else if(user.hand.fullHouse()){
+      user.balance*=10;
+    }
+    else if(user.hand.flush()){
+      user.balance*=5;
+    }
+    else if(user.hand.threeOfAKind()){
+      user.balance*=4;
+    }
+    else if(user.hand.twoPair()){
+      user.balance*=3;
+    }
+    else if(user.hand.pair()){
+      user.balance*=1;
+    }
+    else{
+      user.balance*=0;
+    }
+    return user.balance;
+  }
+
+  //*Royal Flush: five consecutive denomination cards of the same suit,
+  //*starting from 10 and ending with an ace
+  public boolean royalFlush(){
+    while(sameSuit(myCards)){
+      int theSuit=myCards.getCard(0).getSuit();
+      if(myCards.isPresent(10,theSuit)&&myCards.isPresent(11,theSuit)&&myCards.isPresent(12,theSuit)&&myCards.isPresent(13,theSuit)&&myCards.isPresent(1,theSuit)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //*Straight Flush: five consecutive denomination cards of the same suit.
+  public boolean straightFlush(){
+    int straightSuit=myCards.getCard(0).getSuit();
+    if(sameSuit(myCards)&&inOrder(myCards)){
+      return true;
+    }
+    else if(myCards.isPresent(10,straightSuit)&&myCards.isPresent(11,straightSuit)&&myCards.isPresent(12,straightSuit)&&myCards.isPresent(13,straightSuit)&&myCards.isPresent(1,straightSuit)){
+      return true;
+    }
+    return false;
+  }
+
+  //*Four of a kind: four cards of the same denomination.
+  public boolean fourOfAKind(){
+    int count=0;
+    cardValues=numberOfCards(myCards);
+    for(int i=0;i<13;i++){
+      if(cardValues[i]==4){
+        count++;
+      }
+    }
+    if(count==1){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  //Full House: a set of three cards of the same denomination plus
+  //*a set of two cards of the same denomination.
+  public boolean fullHouse(){
+    if(this.pair()&&this.threeOfAKind()){
+      return true;
+    }
+      return false;
+  }
+
+  //*Flush: five non-consecutive denomination cards of the same suit.
+  public boolean flush(){
+    if(sameSuit(myCards)){
+      return true;
+    }
+    return false;
+  }
+
+  //*Straight: five consecutive denomination cards of different suit.
+  public boolean straight(){
+    if(inOrder(myCards)){
+      return true;
+    }
+    return false;
+  }
+
+  //*Three of a Kind: three cards of the same denomination.
+  public boolean threeOfAKind(){
+    int count=0;
+    cardValues=numberOfCards(myCards);
+    for(int i=0;i<13;i++){
+      if(cardValues[i]==3){
+        count++;
+      }
+    }
+    if(count==1){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  //*Two Pair: two sets of pairs of the same card denomination.
+  public boolean twoPair(){
+    int count=0;
+    cardValues=numberOfCards(myCards);
+    for(int i=0;i<13;i++){
+      if(cardValues[i]==2){
+        count++;
+      }
+    }
+    if(count==2){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  //*Jacks or Better: a pair pays out only if the cards in the pair are Jacks,
+  //*Queens, Kings, or Aces. Lower pairs do not pay out.
+  public boolean pair(){
+    int count=0;
+    cardValues=numberOfCards(myCards);
+    for(int i=10;i<13;i++){ //Loops through Jack or better
+      if(cardValues[i]==2){
+        count++;
+      }
+    }
+    if(cardValues[0]==2){ //Checks for an Ace pair
+      count++;
+    }
+    if(count==1){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+
+  //*Helper methods
+
+  //*Checks if all cards in Hand are of the same suit
+  public boolean sameSuit(Hand myHand){
+    int suit1=myHand.getCard(0).getSuit();
+    int suit2=myHand.getCard(1).getSuit();
+    int suit3=myHand.getCard(2).getSuit();
+    int suit4=myHand.getCard(3).getSuit();
+    int suit5=myHand.getCard(4).getSuit();
+    if(suit1==100&&suit2==100&&suit3==100&&suit4==100&&suit5==100)
+      return true;
+    else if(suit1==101&&suit2==101&&suit3==101&&suit4==101&&suit5==101)
+      return true;
+    else if(suit1==102&&suit2==102&&suit3==102&&suit4==102&&suit5==102)
+      return true;
+    else if(suit1==103&&suit2==103&&suit3==103&&suit4==103&&suit5==103)
+      return true;
+    else
+      return false;
+  }
+
+  //*Checks if all cards in Hand are in ascending order
+  public boolean inOrder(Hand myHand){
+    int[] numbers=new int[MAX_SIZE];
+    int[] order=new int[MAX_SIZE];
+    for(int i=0;i<MAX_SIZE;i++){
+      order[i]=i;
+    }
+    for(int i=0;i<MAX_SIZE;i++){
+      numbers[i]=myHand.getCard(i).getRank();
+    }
+    Arrays.sort(numbers);
+    for(int i=0;i<MAX_SIZE;i++){
+      numbers[i]=numbers[i]-numbers[0];
+    }
+    if(numbers==order){
+      return true;
+    }
+    return false;
+  }
+
+  //*Returns an integer array
   public int[] numberOfCards(Hand myHand){
-    cardValues=new int[13];
+    int[] cardNumbers=new int[13];
     int ace=0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0,ten=0,jack=0,queen=0,king=0;
     for(int i=0;i<5;i++){
       switch(myHand.getCard(i).getRank()){
@@ -67,133 +267,8 @@ class Score{
       }
       //String nothing = currentCards[i].toString();
     }
-    return cardValues;
+    return cardNumbers;
   }
-
-  public boolean royalFlush(){
-    while(sameSuit(myCards)){
-      int theSuit=myCards.getCard(0).getSuit();
-      if(myCards.isPresent(10,theSuit)&&myCards.isPresent(11,theSuit)&&myCards.isPresent(12,theSuit)&&myCards.isPresent(13,theSuit)&&myCards.isPresent(1,theSuit)){
-        return true;
-      }
-    }
-    return false;
-  }
-
-  public boolean sameSuit(Hand myHand){
-    int suit1=myHand.getCard(0).getSuit();
-    int suit2=myHand.getCard(1).getSuit();
-    int suit3=myHand.getCard(2).getSuit();
-    int suit4=myHand.getCard(3).getSuit();
-    int suit5=myHand.getCard(4).getSuit();
-    if(suit1==100&&suit2==100&&suit3==100&&suit4==100&&suit5==100)
-      return true;
-    else if(suit1==101&&suit2==101&&suit3==101&&suit4==101&&suit5==101)
-      return true;
-    else if(suit1==102&&suit2==102&&suit3==102&&suit4==102&&suit5==102)
-      return true;
-    else if(suit1==103&&suit2==103&&suit3==103&&suit4==103&&suit5==103)
-      return true;
-    else
-      return false;
-  }
-
-  public boolean inOrder(Hand myHand){
-    int[] numbers=new int[MAX_SIZE];
-    int[] order=new int[MAX_SIZE];
-    for(int i=0;i<MAX_SIZE;i++){
-      order[i]=i;
-    }
-    numbers=numberOfCards(myHand);
-    Arrays.sort(numbers);
-    for(int i=0;i<MAX_SIZE;i++){
-      numbers[i]=numbers[i]-numbers[0];
-    }
-    if(numbers==order){
-      return true;
-    }
-    return false;
-  }
-
-  public boolean straightFlush(){
-    if(sameSuit(myCards)&&inOrder(myCards)){
-      return true;
-    }
-    return false;
-  }
-
-  public boolean fourOfAKind(){
-    int count=0;
-    for(int i=0;i<13;i++){
-      if(cardValues[i]==4){
-        count++;
-      }
-    }
-    if(count==1){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
-  public boolean fullHouse(){
-    return false;
-  }
-  public boolean flush(){
-    return false;
-  }
-  public boolean straight(){
-    return false;
-  }
-  public boolean threeOfAKind(){
-    int count=0;
-    for(int i=0;i<13;i++){
-      if(cardValues[i]==3){
-        count++;
-      }
-    }
-    if(count==1){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
-  public boolean twoPair(){
-    int count=0;
-    for(int i=0;i<13;i++){
-      if(cardValues[i]==2){
-        count++;
-      }
-    }
-    if(count==2){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
-  public boolean pair(){
-    int count=0;
-    for(int i=0;i<13;i++){
-      if(cardValues[i]==2){
-        count++;
-      }
-    }
-    if(count==1){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
-  /*public String winningCombination(){
-
-  }*/
 
   public static void main(String[] args){
     System.out.println("The combination is : ");
